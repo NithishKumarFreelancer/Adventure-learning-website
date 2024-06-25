2b22b87646411b9c9f822eec7a90dbae8716736b
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-06-26 00:30:13 +0530

    navbar component implemented

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from "./Components/Nav"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="app container">
+  <Nav />
+    </div>
+  )
+}
+
+export default App
